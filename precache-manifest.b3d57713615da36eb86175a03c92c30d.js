2230608e84e11d659cb480e917d33dc02f88a599
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2019-10-10 17:25:30 -0300

    Updated gh-pages

---
+++ precache-manifest.b3d57713615da36eb86175a03c92c30d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd9d509a5776c1009b33783db18bf6f",
+    "url": "/sesinfo2019-vue-tictactoe/index.html"
+  },
+  {
+    "revision": "28aade9d83ca85090d7c",
+    "url": "/sesinfo2019-vue-tictactoe/static/css/main.4be0e0c3.chunk.css"
+  },
+  {
+    "revision": "3ccc8c83082dd8c27e71",
+    "url": "/sesinfo2019-vue-tictactoe/static/js/2.f7db762b.chunk.js"
+  },
+  {
+    "revision": "28aade9d83ca85090d7c",
+    "url": "/sesinfo2019-vue-tictactoe/static/js/main.dfa2104e.chunk.js"
+  },
+  {
+    "revision": "9f8192a2c627a0709f8c",
+    "url": "/sesinfo2019-vue-tictactoe/static/js/runtime-main.f04c55ef.js"
+  }
+]);
